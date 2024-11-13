21ba93898bb5de5be8867c14babe9e0aca7b1a56
jacobjeremiahjohnson <jacobjeremiah42@gmail.com>
2024-11-13 16:25:37 -0500

    Added injected css file

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,3 @@
-chrome.storage.local.get(["toggle"]).then((result) => { // If toggle set to true, perform page action
-  console.log(result.toggle)
-
-  if (result.toggle) {
-    editPage()
-  }
-
-})
-
 chrome.runtime.onMessage.addListener( // Whenever extension icon selected, switch toggle variable
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
@@ -25,11 +16,64 @@ chrome.runtime.onMessage.addListener( // Whenever extension icon selected, switc
 
     return true;
   }
-);
+)
+
+chrome.storage.local.get(["toggle"]).then((result) => { // If toggle set to true, perform page action
+  console.log(result.toggle)
+
+  if (result.toggle) {
+    editPage()
+  }
+
+})
 
 function editPage() { // Edit page 
   const header = document.querySelector("header");
-  if (header) {
-    header.parentElement.removeChild(header)
+  const main = document.getElementsByClassName("nH aqk aql bkL")[0]
+
+  //let zoom = (( window.outerWidth - 10 ) / window.innerWidth) * 100;
+
+  if (header && main) { // Check proper elements are present (gg if this doesn't work)
+    
+    const leftbar = main.children[0]
+    const middle = main.children[1]
+
+    leftbar.id = "leftbar"
+    main.id = "main"
+    middle.id = "middle"
+
+    // Make main email section scrollable
+    header.children[1].firstChild.firstChild.parentElement.removeChild(header.children[1].firstChild.firstChild)
+    const selectBar = document.getElementById(":4")
+    const scrollSection = document.getElementById(":3")
+
+    scrollSection.insertBefore(selectBar, scrollSection.firstChild)
+    scrollSection.insertBefore(header, scrollSection.firstChild)
+
+    // const selectBarControls = document.getElementsByClassName("nH aqK")[0]
+    // const accountButton = document.getElementsByClassName("gb_y gb_bd gb_Nf gb_Z")[0]
+    // console.log(accountButton)
+    // console.log(selectBar)
+
+    // selectBarControls.insertBefore(accountButton, selectBarControls.firstChild)
+
+
+    // Add search bar functionality
+    //const searchBar = document.getElementsByClassName("gb_we")[0]
+    //leftbar.insertBefore(searchBar, leftbar.children[2])
+    
+    const emails = document.querySelector("td")
+
+    console.log(emails)
+    for (let i = 0; i < emails.length; i++){
+      console.log(emails[i])
+      emails[i].children[4].style.padding = "0px 0px 0px 0px"
+    }
+
+    // header.parentElement.removeChild(header)
+    const rightbar = main.children[2]
+    rightbar.parentElement.removeChild(rightbar)
+    rightbar.id = "rightbar"
+    // selectBar.parentElement.removeChild(selectBar)
   }
 }
